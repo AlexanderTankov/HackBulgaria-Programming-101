3ff7b625a4b5239d542da1d3f2d3ad2d051da358
Alexandar <AlexandarTankov@gmail.com>
2014-10-20 21:41:07 +0300

    Compleate berserk_fractor in orcs

--- Week 2/hero_test.py
+++ Week 2/hero_test.py
@@ -4,13 +4,13 @@ from hero import Hero
 
 
 class TestHero(unittest.TestCase):
+    def setUp(self):
+        self.bron_hero = Hero("Bron", 100, "DragonSlayer")
 
     def test_hero_init(self):
-        self.bron_hero = Hero("Bron", 100, "DragonSlayer")
         self.assertEqual(self.bron_hero.nickname, "DragonSlayer")
 
     def test_known_as(self):
-        self.bron_hero = Hero("Bron", 100, "DragonSlayer")
         self.assertEqual(self.bron_hero.known_as(), "Bron the DragonSlayer")
 
 if __name__ == '__main__':
--- Week 2/orc_test.py
+++ Week 2/orc_test.py
@@ -4,10 +4,15 @@ from orc import Orc
 
 
 class TestOrc(unittest.TestCase):
+    def setUp(self):
+        self.bron_orc = Orc("Bron", 100, 1.3)
 
     def test_orc_init(self):
-        self.bron_orc = Orc("Bron", 100, 1)
-        self.assertEqual(self.bron_orc.berserk_factor, 1)
+        self.assertEqual(self.bron_orc.berserk_factor, 1.3)
+
+    def test_init_incorect_berserk_factor(self):
+        with self.assertRaises(ValueError):
+            Orc("Bron", 100, 2.3)
 
 if __name__ == '__main__':
     unittest.main()
