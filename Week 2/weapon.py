fc32907f2d8dd38efdd132743a442a32116e303a
Alexandar <AlexandarTankov@gmail.com>
2014-10-22 02:06:12 +0300

    Work on class Weapon

---
+++ Week 2/weapon.py
@@ -0,0 +1,8 @@
+class Weapon:
+
+    def __init__(self, type, damage, critical_strike_percent):
+        self.type = type
+        self.damage = damage
+        self.critical_strike_percent = critical_strike_percent
+
+    
---
+++ Week 2/weapon_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from weapon import Weapon
+
+
+class TestWeapon(unittest.TestCase):
+
+    def setUp(self):
+        self.axe = Weapon("Mighty Axe", 25, 0.2)
+
+    def test_entity_init(self):
+        self.assertEqual(self.axe.type, "Mighty Axe")
+        self.assertEqual(self.axe.damage, 25)
+        self.assertEqual(self.axe.critical_strike_percent, 0.2)
+
+if __name__ == '__main__':
+    unittest.main()
